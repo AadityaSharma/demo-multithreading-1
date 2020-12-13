a3531c61913e272e9b7f0a82793f8fa9b14ecb33
Aaditya Sharma <accolite-asharma@evernote.com>
2020-12-13 15:14:18 +0530

    Demo - thread termination and daemon threads

---
+++ src/examples/E5BlockingTask.java
@@ -0,0 +1,33 @@
+package examples;
+
+public class E5BlockingTask {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new BlockingTask());
+        thread.start();
+        thread.interrupt();
+    }
+
+    private static class BlockingTask implements Runnable {
+
+        /**
+         * When an object implementing interface <code>Runnable</code> is used
+         * to create a thread, starting the thread causes the object's
+         * <code>run</code> method to be called in that separately executing
+         * thread.
+         * <p>
+         * The general contract of the method <code>run</code> is that it may
+         * take any action whatsoever.
+         *
+         * @see Thread#run()
+         */
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(500000);
+            } catch (InterruptedException e) {
+                System.out.println("Exiting Block Thread");
+            }
+        }
+    }
+}
---
+++ src/examples/E6LongComputationTask.java
@@ -0,0 +1,55 @@
+package examples;
+
+import java.math.BigInteger;
+
+public class E6LongComputationTask {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new LongComputationTask(new BigInteger("21"), new BigInteger("10000")));
+
+//        thread.setDaemon(true);
+        thread.start();
+        Thread.sleep(100);
+        thread.interrupt();
+    }
+
+    private static class LongComputationTask implements Runnable {
+        private BigInteger base;
+        private BigInteger power;
+
+        public LongComputationTask(BigInteger base, BigInteger power) {
+            this.base = base;
+            this.power = power;
+        }
+
+        /**
+         * When an object implementing interface <code>Runnable</code> is used
+         * to create a thread, starting the thread causes the object's
+         * <code>run</code> method to be called in that separately executing
+         * thread.
+         * <p>
+         * The general contract of the method <code>run</code> is that it may
+         * take any action whatsoever.
+         *
+         * @see Thread#run()
+         */
+        @Override
+        public void run() {
+            System.out.println(base + "^" + power + " = " + pow(base, power));
+        }
+
+        private BigInteger pow(BigInteger base, BigInteger power) {
+            BigInteger result = BigInteger.ONE;
+
+            for (BigInteger i = BigInteger.ZERO; i.compareTo(power) != 0; i = i.add(BigInteger.ONE)) {
+                if (Thread.currentThread().isInterrupted()) {
+                    System.out.println("Prematurely interrupted computation");
+                    return BigInteger.ZERO;
+                }
+                result = result.multiply(base);
+            }
+
+            return result;
+        }
+    }
+}
